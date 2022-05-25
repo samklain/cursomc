3bbba871a56634774e1f8e0944198537a767b9e1
Samuel Klain <l>
2022-05-24 23:04:55 -0300

    Projeto criado
    Please enter the commit message for your changes. Lines starting

---
+++ src/test/java/com/klain/curso/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klain.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
